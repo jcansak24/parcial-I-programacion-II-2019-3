252fdc1bd0302c5f51bd99387ee1de8e0ea715f6
Jimer.Sayago <jimer.sayago@unet.edu.ve>
2020-02-16 01:38:24 -0400

    primer commit parcial

---
+++ FacilDecir/FacilDecir/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FacilDecir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreArchivo = "facil.txt";
+            string linea;
+            string patronVocales = $"[{"a"}]|[{"e"}]|[{"i"}]|[{"o"}]|[{"u"}]";
+            string patronVocSegPermitidas = $"[{"e"}]|[{"o"}]";
+
+            TextReader leerArch = new StreamReader(nombreArchivo);
+            linea = leerArch.ReadLine();
+
+            Console.WriteLine("***Generador de passwords***");
+            while (linea != "end")
+            {
+                int contVocales = 1, contConsonante = 1;
+                Regex miRegex = new Regex(patronVocales);
+                MatchCollection elMatch = miRegex.Matches(linea);
+                if (elMatch.Count > 0)
+                {
+                    for (int i = 1; i< linea.Length; i++)
+                    {
+                        if (linea.Length == 1 && elMatch.Count > 0) Console.WriteLine($"<{linea}> is acceptable");
+                        else {                            
+                            if (linea.Length > 1 && (linea.ElementAt<char>(i - 1) != linea.ElementAt<char>(i)))
+                            {
+                                char letra = linea.ElementAt<char>(i - 1);
+                                char letra2 = linea.ElementAt<char>(i);
+                                Regex miRegex2 = new Regex(patronVocales);
+                                MatchCollection elMatch2 = miRegex.Matches(letra.ToString());
+                                Regex miRegex3 = new Regex(patronVocales);
+                                MatchCollection elMatch3 = miRegex.Matches(letra2.ToString());
+                                if (elMatch2.Count > 0 && elMatch3.Count > 0) contVocales++;                                
+                                if (elMatch2.Count == 0 && elMatch3.Count == 0) contConsonante++;                                
+                                if((elMatch2.Count == 0 && elMatch3.Count > 0) || (elMatch2.Count > 0 && elMatch3.Count == 0))
+                                {
+                                    if (contVocales < 3 && contConsonante < 3)  contConsonante = contVocales = 1; 
+                                }
+                                                               
+                            } else
+                            {
+                                if (linea.Length>1 && (linea.ElementAt<char>(i - 1) == linea.ElementAt<char>(i)))
+                                {
+                                    char letra3 = linea.ElementAt<char>(i);
+                                    Regex miRegex4 = new Regex(patronVocSegPermitidas);
+                                    MatchCollection elMatch4 = miRegex4.Matches(letra3.ToString());
+                                    if (elMatch4.Count == 0) {
+                                        i = linea.Length - 1;
+                                        contConsonante = contVocales = 3;
+                                    }
+                                    else { contVocales++; }                                                                
+                                }                                
+                            }
+                        }                       
+                    }
+                    if (contVocales < 3 && contConsonante < 3) Console.WriteLine($"<{linea}> is acceptable");                    
+                    else Console.WriteLine($"<{linea}> is not acceptable");                    
+                }
+                else Console.WriteLine($"<{linea}> is not acceptable");
+                linea = leerArch.ReadLine();
+            }
+            leerArch.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Misiones/Misiones/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/*Realizado por Jimer Sayago CIV- 24611409*/
+
+namespace Misiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCasos, cont = 0;
+            string nombreArchivo = "misiones.txt";
+            string linea;
+
+            TextReader leerArch = new StreamReader(nombreArchivo);
+            numCasos = int.Parse(leerArch.ReadLine());
+            linea = leerArch.ReadLine();
+            int[] navesxCasos = new int[numCasos];
+            if (numCasos < 1 || numCasos > 50)
+            {
+                Console.WriteLine("El numero de casos excede el maximo permitido. Intente evaluar hasta 50 casos");
+                linea = null;
+            }
+            while (linea != null)
+            {
+                string[] num = linea.Split();
+                int naves = int.Parse(num[0]);
+                int dist = int.Parse(num[1]);
+                linea = leerArch.ReadLine();
+                if ((naves < 1 || naves > 100) || (dist < 1 || dist > 999999)) {
+                    Console.WriteLine("El numero de naves o distancia ha excedido el maximo permitido");
+                    linea = null; break;
+                }
+                int contNaves = 0;                
+                for (int i = 0; i < naves; i++)
+                {
+                    string[] caso = linea.Split();
+                    float veloc = float.Parse(caso[0]);
+                    float peso = float.Parse(caso[1]);
+                    float consumo = float.Parse(caso[2]);
+                    if ((veloc < 1 || veloc > 1000) || (peso < 1 || peso > 1000) || (consumo < 1 || consumo > 1000)){
+                        Console.WriteLine("La cantidad solicitada excede los limites maximos permitidos."); break;
+                    }
+                    float cantHorasDestino = dist / veloc;
+                    float consTot = cantHorasDestino * consumo;
+                    if (consTot <= peso) contNaves++; 
+                    linea = leerArch.ReadLine();
+                }
+                navesxCasos[cont++] = contNaves;   
+            }
+            leerArch.Close();
+            Console.WriteLine("***Naves que llegaron al sitio de expedicion***");
+            foreach (int navesMision in navesxCasos) Console.WriteLine($"\t{navesMision}");
+            Console.ReadLine();
+        }
+    }
+}
